30c88bf81c586ba690ea2714b264a13e41d5e130
zhangdingping <zhangdingping@oceanum.cn>
2019-10-25 18:22:30 +0800

    网关搭建

---
+++ src/main/java/com/order/orderzuulservice/OrderZuulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.order.orderzuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderZuulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZuulServiceApplication.class, args);
+    }
+
+}
