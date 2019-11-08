3dd770eab16248dc36b6ee3b8c24b8da3f202713
zhangdingping <zhangdingping@oceanum.cn>
2019-11-08 17:02:19 +0800

    完工“

--- src/main/java/com/order/orderzuulservice/remote/WebRpcRemote.java
+++ src/main/java/com/order/orderzuulservice/remote/WebRpcRemote.java
@@ -3,7 +3,6 @@ package com.order.orderzuulservice.remote;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
